190f0bf65654be46920b6eb710426420acb3670d
iyoo14 <iyoo14@gmail.com>
2020-01-19 23:45:03 +0900

    gologger バグ修正

--- gologger.go
+++ gologger.go
@@ -14,8 +14,11 @@ type Logger struct {
 
 func NewLogger(filepath string, prefix string) *Logger {
 	n := time.Now()
-	ff := fmt.Sprintf("%04d%02d%02d", n.Year(), n.Month(), n.Day())
-	file, _ := os.OpenFile(filepath+"/"+ff+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	ff := fmt.Sprintf("%s-%04d%02d%02d.log", prefix, n.Year(), n.Month(), n.Day())
+	file, err := os.OpenFile(filepath+"/"+ff, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
 	logger := log.New(file, "["+prefix+"]", log.LstdFlags)
 	lg := new(Logger)
 	lg.fd = file
@@ -25,15 +28,19 @@ func NewLogger(filepath string, prefix string) *Logger {
 }
 
 func (lg *Logger) Println(v ...interface{}) {
-	lg.logger.Println(v)
+	lg.logger.Println(v...)
+}
+
+func (lg *Logger) Printf(format string, v ...interface{}) {
+	lg.logger.Printf(format, v...)
 }
 
 func (lg *Logger) Fatal(v ...interface{}) {
-	lg.logger.Fatal(v)
+	lg.logger.Fatal(v...)
 }
 
 func (lg *Logger) Fatalf(format string, v ...interface{}) {
-	lg.logger.Fatalf(format)
+	lg.logger.Fatalf(format, v...)
 }
 
 func (lg *Logger) Close() {
--- gologger_test.go
+++ gologger_test.go
@@ -1,16 +1,17 @@
 package gologger
 
 import (
-	"log"
 	"os"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
-	logger := NewLogger(gopath+"log", "gologger")
-	log.Println("test")
+	logger := NewLogger(gopath+"/log", "gologger")
+	//logger.Close()
+	//log.Println("test")
 	//logger.Close()
 	logger.Println("start test.")
+	logger.Printf("%d, %s", 1, "test")
 	logger.Println("end test")
 }
