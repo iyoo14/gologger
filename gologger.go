8c161eef2b1a19f13722844d1b3e2efa8b74756e
iyoo14 <iyoo14@gmail.com>
2020-01-19 12:52:11 +0900

    gologger structに変更

--- gologger.go
+++ gologger.go
@@ -7,13 +7,35 @@ import (
 	"time"
 )
 
-func Logger(filepath string, prefix string) *log.Logger {
+type Logger struct {
+	fd     *os.File
+	logger *log.Logger
+}
+
+func NewLogger(filepath string, prefix string) *Logger {
 	n := time.Now()
 	ff := fmt.Sprintf("%04d%02d%02d", n.Year(), n.Month(), n.Day())
 	file, _ := os.OpenFile(filepath+"/"+ff+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	defer func() {
-		file.Close()
-	}()
 	logger := log.New(file, "["+prefix+"]", log.LstdFlags)
-	return logger
+	lg := new(Logger)
+	lg.fd = file
+	lg.logger = logger
+
+	return lg
+}
+
+func (lg *Logger) Println(v ...interface{}) {
+	lg.logger.Println(v)
+}
+
+func (lg *Logger) Fatal(v ...interface{}) {
+	lg.logger.Fatal(v)
+}
+
+func (lg *Logger) Fatalf(format string, v ...interface{}) {
+	lg.logger.Fatalf(format)
+}
+
+func (lg *Logger) Close() {
+	defer lg.fd.Close()
 }
--- gologger_test.go
+++ gologger_test.go
@@ -1,13 +1,16 @@
 package gologger
 
 import (
+	"log"
 	"os"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
-	logger := Logger(gopath+"log", "gologger")
+	logger := NewLogger(gopath+"log", "gologger")
+	log.Println("test")
+	//logger.Close()
 	logger.Println("start test.")
 	logger.Println("end test")
 }
